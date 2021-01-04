886f377dd1f4ca1470ca8e4d3af5c78d886788b4
Spiderfav <ruifavinha2002@gmail.com>
2021-01-04 15:35:25 +0000

    Not many changes

--- Main GO Code/web.go
+++ Main GO Code/web.go
@@ -9,7 +9,7 @@ import (
 	This is the start of the web server with the REST API
 */
 func main() {
-	http.HandleFunc("/hello", func(rw http.ResponseWriter, req *http.Request) {
+	http.HandleFunc("/login", func(rw http.ResponseWriter, req *http.Request) {
 		name := req.URL.Query().Get("name")
 		rw.Write([]byte(fmt.Sprintf("Hello, %s", name)))
 	})
