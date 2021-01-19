8518e9e808d0fc028271a542767b9ab6318fc952
Spiderfav <ruifavinha2002@gmail.com>
2021-01-19 20:13:36 +0000

    Trying to convert some code

---
+++ PSN API/User/User.go
@@ -0,0 +1,24 @@
+package user
+
+import "encoding/json"
+
+type user struct {
+	Oauth        string
+	RefreshToken string
+}
+
+type message struct {
+	MessageStart string
+	Body         string
+}
+
+func me(user) {
+	var usersUrl string = "https://us-prof.np.community.playstation.net/userProfile/v1/users/"
+	var endpoint string = "me/profile2?fields=npId,onlineId,avatarUrls,plus,aboutMe,languagesUsed,trophySummary(@default,progress,earnedTrophies),isOfficiallyVerified,personalDetail(@default,profilePictureUrls),personalDetailSharing,personalDetailSharingRequestMessageFlag,primaryOnlineStatus,presences(@titleInfo,hasBroadcastData),friendRelation,requestMessageFlag,blocking,mutualFriendsCount,following,followerCount,friendsCount,followingUsersCount&avatarSizes=m,xl&profilePictureSizes=m,xl&languagesUsedLanguageSet=set3&psVitaTitleIcon=circled&titleIconSize=s"
+	header := message{"Authorisation", "Bearer " + user.Oauth}
+	b, err := json.Marshal(header)
+	if err != nil {
+		panic(err)
+	}
+
+}
