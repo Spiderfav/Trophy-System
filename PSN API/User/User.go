768a8c430b2a25c66fb82d9a7642f9d8e631405f
Spiderfav <ruifavinha2002@gmail.com>
2021-01-20 21:19:28 +0000

    "Finished"User file

--- PSN API/User/User.go
+++ PSN API/User/User.go
@@ -1,24 +1,36 @@
 package user
 
-import "encoding/json"
+import (
+	"io/ioutil"
+	"net/http"
+)
 
-type user struct {
+type User struct {
 	Oauth        string
 	RefreshToken string
 }
 
-type message struct {
+type Message struct {
 	MessageStart string
 	Body         string
 }
 
-func me(u user, m message) {
-	var usersUrl string = "https://us-prof.np.community.playstation.net/userProfile/v1/users/"
+func me(oauth string, token string) (data []byte) {
+
+	userMade := User{oauth, token}
+	var usersURL string = "https://us-prof.np.community.playstation.net/userProfile/v1/users/"
 	var endpoint string = "me/profile2?fields=npId,onlineId,avatarUrls,plus,aboutMe,languagesUsed,trophySummary(@default,progress,earnedTrophies),isOfficiallyVerified,personalDetail(@default,profilePictureUrls),personalDetailSharing,personalDetailSharingRequestMessageFlag,primaryOnlineStatus,presences(@titleInfo,hasBroadcastData),friendRelation,requestMessageFlag,blocking,mutualFriendsCount,following,followerCount,friendsCount,followingUsersCount&avatarSizes=m,xl&profilePictureSizes=m,xl&languagesUsedLanguageSet=set3&psVitaTitleIcon=circled&titleIconSize=s"
-	header := message{"Authorisation", "Bearer " + u.Oauth}
-	b, err := json.Marshal(header)
+
+	req, err := http.NewRequest("GET", usersURL+endpoint, nil)
+	req.Header.Add("Authorisation", "Bearer "+userMade.Oauth)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		panic(err)
 	}
+	data, _ = ioutil.ReadAll(resp.Body)
+
+	resp.Body.Close()
+
+	return data
 
 }
