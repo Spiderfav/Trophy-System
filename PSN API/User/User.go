09fd80cda8d6e2cafe0d356165b0f1eaec4b9a3c
Spiderfav <ruifavinha2002@gmail.com>
2021-01-20 15:31:44 +0000

    Idek

--- PSN API/User/User.go
+++ PSN API/User/User.go
@@ -12,10 +12,10 @@ type message struct {
 	Body         string
 }
 
-func me(user) {
+func me(u user, m message) {
 	var usersUrl string = "https://us-prof.np.community.playstation.net/userProfile/v1/users/"
 	var endpoint string = "me/profile2?fields=npId,onlineId,avatarUrls,plus,aboutMe,languagesUsed,trophySummary(@default,progress,earnedTrophies),isOfficiallyVerified,personalDetail(@default,profilePictureUrls),personalDetailSharing,personalDetailSharingRequestMessageFlag,primaryOnlineStatus,presences(@titleInfo,hasBroadcastData),friendRelation,requestMessageFlag,blocking,mutualFriendsCount,following,followerCount,friendsCount,followingUsersCount&avatarSizes=m,xl&profilePictureSizes=m,xl&languagesUsedLanguageSet=set3&psVitaTitleIcon=circled&titleIconSize=s"
-	header := message{"Authorisation", "Bearer " + user.Oauth}
+	header := message{"Authorisation", "Bearer " + u.Oauth}
 	b, err := json.Marshal(header)
 	if err != nil {
 		panic(err)
