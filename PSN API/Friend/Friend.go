91ee6bc81773c07c047b534061ae68d196ded0cd
Spiderfav <ruifavinha2002@gmail.com>
2021-01-23 20:37:39 +0000

    Added friend

---
+++ PSN API/Friend/Friend.go
@@ -0,0 +1,26 @@
+package Friend
+
+import "encoding/json"
+
+type user struct {
+	Oauth        string
+	RefreshToken string
+}
+
+type message struct {
+	MessageStart string
+	Body         string
+}
+
+func my_friends(u user, m message) {
+	var usersUrl string = "https://us-prof.np.community.playstation.net/userProfile/v1/users/"
+	var endpoint string = "me/profile2?fields=npId,onlineId,avatarUrls,plus,aboutMe,languagesUsed,trophySummary(@default,progress,earnedTrophies),isOfficiallyVerified,personalDetail(@default,profilePictureUrls),personalDetailSharing,personalDetailSharingRequestMessageFlag,primaryOnlineStatus,presences(@titleInfo,hasBroadcastData),friendRelation,requestMessageFlag,blocking,mutualFriendsCount,following,followerCount,friendsCount,followingUsersCount&avatarSizes=m,xl&profilePictureSizes=m,xl&languagesUsedLanguageSet=set3&psVitaTitleIcon=circled&titleIconSize=s"
+	header := message{"Authorisation", "Bearer " + u.Oauth}
+	var filter string = "Online"
+	var limit int = 36
+	b, err := json.Marshal(header)
+	if err != nil {
+		panic(err)
+	}
+
+}
