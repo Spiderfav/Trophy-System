e5dd0d641a018387e2d08d8226026050391c488f
Spiderfav <ruifavinha2002@gmail.com>
2021-02-02 16:04:22 +0000

    Printing Achievements

--- Xbox/connect.go
+++ Xbox/connect.go
@@ -13,4 +13,25 @@ func main() {
 	}
 	gamertag := client.Gamertag()
 	fmt.Println(gamertag)
+	client2, err := xbl.Login("Jessgood123@hotmail.co.uk", "Daddy2020?!")
+	if err != nil {
+		panic(err)
+	}
+	gamertag2 := client2.Gamertag()
+	fmt.Println(gamertag2)
+	jessXID := client2.UserID()
+
+	profile, err := client2.Profile(gamertag2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(profile)
+
+	achievements, err := client2.Achievements(jessXID)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(achievements)
+
 }
